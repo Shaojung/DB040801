ed14d97585073081f6e26f3fcb92dace7d35451b
Shaojung <shaojung@gmail.com>
2017-04-08 13:51:06 +0800

    copydatabase

--- app/src/main/java/com/example/teacher/db040801/MainActivity.java
+++ app/src/main/java/com/example/teacher/db040801/MainActivity.java
@@ -16,7 +16,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        try {
+            copyDatabase();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void copyDatabase() throws IOException {
